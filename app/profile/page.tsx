1ad16a895097d6507863950dd60d8cdee0673cf0
mohadese.gh <87853947+lmohade3l@users.noreply.github.com>
2026-09-15 16:52:54 +0330

    feat: profile: end of the day section ui

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,5 +1,6 @@
 import { THEME_OPTIONS } from '@/lib/theme'
 import s from './Profile.module.css'
+import ChevronRight from '@/public/icons/chevron-right.svg'
 
 export default function Profile() {
     const active = THEME_OPTIONS[0]
@@ -14,8 +15,8 @@ export default function Profile() {
                 <div className={s.itemsContainer}>
                     {/* theme */}
                     <div className={s.itemBox}>
-                        <p className={s.itemLabel}>Theme</p>
-                        <p className={s.itemSupport}>System follows your device setting</p>
+                        <div className={s.itemLabel}>Theme</div>
+                        <div className={s.itemSupport}>System follows your device setting</div>
                         <div className={s.themeOptionsContainer}>
                             {THEME_OPTIONS?.map((o) => (
                                 <div key={o} className={`${s.themeOption} ${o===active ? s.active : ''}`}>{o}</div>
@@ -23,10 +24,22 @@ export default function Profile() {
                         </div>
                     </div>
                     
+                    <div className={s.divider}/>
 
                     {/* end of the day */}
                     <div className={s.itemBox}>
+                        <div className={s.list}>
+                            <div className={s.listLeadingContainer}>
+                                <div className={s.itemLabel}>End of day</div>
+                                <div className={s.itemSupport}>When a day rolls over</div>
+                            </div>
 
+                            <div className={s.listTrailingContainer}>
+                                <div>{'3:00 AM'}</div>
+                                <ChevronRight />
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
